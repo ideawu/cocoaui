0271022e2cf83bbf91d5022889c73e3baf836570
ideawu <ideawu@me>
2015-12-01 22:13:36 +0800

    update

--- IKit/Test/TestController.h
+++ IKit/Test/TestController.h
@@ -1,10 +1,11 @@
-//
-//  TestController.h
-//  IKit
-//
-//  Created by ideawu on 12/1/15.
-//  Copyright © 2015 ideawu. All rights reserved.
-//
+/*
+ Copyright (c) 2015 ideawu. All rights reserved.
+ Use of this source code is governed by a license that can be
+ found in the LICENSE file.
+
+ @author:  ideawu
+ @website: http://www.cocoaui.com/
+ */
 
 #import "IKit/IKit.h"
 
