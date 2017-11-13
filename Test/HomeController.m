11f4c202db9d33bb92f3e1686fafe18f79ebd871
ideawu <ideawu@me>
2017-11-13 12:06:01 +0800

    refactor Test/HomeController

--- Test/HomeController.m
+++ Test/HomeController.m
@@ -20,6 +20,11 @@
 #import "ISelectDemo.h"
 #import "ITableNestedDemo.h"
 
+@interface HomeController(){
+	NSArray *_config;
+}
+@end
+
 @implementation HomeController
 
 - (void)add_btn:(NSString *)text{
@@ -48,32 +53,33 @@
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	
-	NSString *css = @"   .a \n{\n    width: 100%; } ";
-	IStyleSheet *sheet = [[IStyleSheet alloc] init];
-	[sheet parseCss:css baseUrl:nil];
-	
 	self.navigationItem.title = @"Home";
 	self.navigationController.navigationBar.translucent = NO;
 	self.view.backgroundColor = [UIColor groupTableViewBackgroundColor];
 	
-	[self add_btn:@"Login"];
-	[self add_btn:@"ITableFixedHeaderDemo"];
-	[self add_btn:@"ITablePullRefreshDemo"];
-	[self add_btn:@"ITableClickToRefresh"];
-	[self add_btn:@"ITableInUIView"];
-	[self add_btn:@"ITableNestedDemo"];
-	[self add_btn:@"Test"];
-	[self add_btn:@"IPopoverDemo"];
-	[self add_btn:@"ISelectDemo"];
-
-	[self addSeparator:@"height: 18;"];
-	[self add_btn:@"Detect memory leak"];
+	_config = @[
+				@[@"Login Page", [LoginController class]],
+				@[@"ITable with Fixed Header", [ITableFixedHeaderDemo class]],
+				@[@"ITable Pull to Refresh", [ITablePullRefreshDemo class]],
+				@[@"ITable Click to Refresh", [ITableClickToRefresh class]],
+				@[@"ITable in UIView", [ITableInUIView class]],
+				@[@"Nested ITable", [ITableNestedDemo class]],
+				@[@"divider", @"height: 18;"],
+				@[@"Popover", [IPopoverDemo class]],
+				@[@"Select/Dropdown Menu", [ISelectDemo class]],
+				@[@"divider", @"height: 18;"],
+				@[@"Detect memory leak", [ISelectDemo class]],
+				];
 	
-	[self addSeparator:@"height: 8;"];
-	
-	// 测试内存泄露
-	//[self autoload];
+	for(NSArray *item in _config){
+		NSString *key = item[0];
+		if([key isEqualToString:@"divider"]){
+			NSString *val = [item[1] description];
+			[self addSeparator:val];
+		}else{
+			[self add_btn:key];
+		}
+	}
 }
 
 - (void)onClick:(IView *)view atIndex:(NSUInteger)index{
@@ -83,34 +89,17 @@
 - (void)click:(UIButton *)btn{
 	NSString *text = btn.titleLabel.text;
 	log_debug(@"click %@", text);
+	
 	UIViewController *controller;
-	if([text isEqualToString:@"Login"]){
-		controller = [[LoginController alloc] init];
+	for(NSArray *item in _config){
+		NSString *key = item[0];
+		if([key isEqualToString:text]){
+			Class clz = item[1];
+			controller = [[clz alloc] init];
+			break;
+		}
 	}
-	if([text isEqualToString:@"ITableFixedHeaderDemo"]){
-		controller = [[ITableFixedHeaderDemo alloc] init];
-	}
-	if([text isEqualToString:@"ITablePullRefreshDemo"]){
-		controller = [[ITablePullRefreshDemo alloc] init];
-	}
-	if([text isEqualToString:@"ITableClickToRefresh"]){
-		controller = [[ITableClickToRefresh alloc] init];
-	}
-	if([text isEqualToString:@"Test"]){
-		controller = [[TestController alloc] init];
-	}
-	if([text isEqualToString:@"IPopoverDemo"]){
-		controller = [[IPopoverDemo alloc] init];
-	}
-	if([text isEqualToString:@"ITableInUIView"]){
-		controller = [[ITableInUIView alloc] init];
-	}
-	if([text isEqualToString:@"ISelectDemo"]){
-		controller = [[ISelectDemo alloc] init];
-	}
-	if([text isEqualToString:@"ITableNestedDemo"]){
-		controller = [[ITableNestedDemo alloc] init];
-	}
+
 	if([text isEqualToString:@"Detect memory leak"]){
 		NSString *xml = @"<html><head><title>404 Not Found</title></head><body bgcolor=\"white\"><center><h1>404 Not Found</h1></center><hr/><center>nginx/1.6.2</center><span>a<a>b</a>c</span></body></html>";
 		log_debug(@"start");
